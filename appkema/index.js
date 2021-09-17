52277ce206aacd2db1328601786ce3520654264a
Kemona Amadi-Emina <KemaTheGentleGiant@Kemonas-MacBook-Pro.local>
2021-09-16 22:48:46 -0400

    pushing assignment 4

--- appkema/index.js
+++ appkema/index.js
@@ -1,11 +1,57 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const mariadb = require('mariadb');
+let conn;
+
+async function createConnection() {
+  if(!conn) {
+    try {
+      conn = await mariadb.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: 'Martina+4',
+        database: 'sample',
+        port: 3306,
+        connectionLimit: 5
+    }).getConnection();
+    } catch(err) {
+      console.log('connection not created', err)
+    } finally {
+      if(conn) {
+        conn.end();
+      }
+    }
+
+  }
+}
+
+app.get('/student',async (req, res) => {
+  await createConnection();
+  const val = await conn.query("select * from student;");
+  console.log("val is: ", val);
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(val))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/customer',async (req, res) => {
+  await createConnection();
+  const val = await conn.query("select * from customer;");
+  console.log("val is: ", val);
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(val))
 })
+
+app.get('/company',async (req, res) => {
+  await createConnection();
+  const val = await conn.query("select * from company;");
+  console.log("val is: ", val);
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(val))
+})
+
+
+app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`)
+});
